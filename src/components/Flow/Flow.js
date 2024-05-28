c07a51107e62bf2d1d93588ebdc08d719ddb8170
rushikesh-tk <rushikeshtaksande7@gmail.com>
2024-05-28 12:34:03 +0530

    Flow view added with initial nodes and edges

---
+++ src/components/Flow/Flow.js
@@ -0,0 +1,85 @@
+import React, { useCallback, useState } from "react";
+import ReactFlow, {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  Controls,
+  MiniMap,
+  Background,
+} from "reactflow";
+import "reactflow/dist/style.css";
+import TextNode from "../TextNode/TextNode";
+
+const rfStyle = {
+  backgroundColor: "#ffffff",
+};
+
+const initialNodes = [
+  {
+    id: "node-1",
+    type: "textUpdater",
+    position: { x: 0, y: 0 },
+    data: { value: 123 },
+  },
+  {
+    id: "node-2",
+    type: "textUpdater",
+    targetPosition: "top",
+    position: { x: 0, y: 200 },
+    data: { label: "node 2" },
+  },
+  {
+    id: "node-3",
+    type: "output",
+    targetPosition: "top",
+    position: { x: 200, y: 200 },
+    data: { label: "node 3" },
+  },
+];
+
+const initialEdges = [
+  { id: "edge-1", source: "node-1", target: "node-2", sourceHandle: "a" },
+  { id: "edge-2", source: "node-1", target: "node-3", sourceHandle: "b" },
+  { id: "edge-3", source: "node-1", target: "node-3", sourceHandle: "a" },
+];
+
+const nodeTypes = { textUpdater: TextNode };
+
+const Flow = () => {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ height: "90vh", width: "75%" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+        style={rfStyle}
+      >
+        <Controls />
+        {/* <MiniMap /> */}
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default Flow;
