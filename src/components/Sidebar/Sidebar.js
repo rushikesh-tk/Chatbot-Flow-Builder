64fabec922349c902cf78cd257cba3e146fd3718
rushikesh-tk <rushikeshtaksande7@gmail.com>
2024-05-29 11:46:29 +0530

    Sidebar made extensible

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./Sidebar.css";
+import { MessageTwoTone } from "@ant-design/icons";
+
+const DraggableNode = (props) => {
+  const { onDragStart, nodeType } = props;
+  return (
+    <div
+      className="dndnode input"
+      onDragStart={(event) => onDragStart(event, nodeType)}
+      draggable
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <MessageTwoTone style={{ fontSize: "3vh", color: "#0041d0" }} />
+      <div className="msg-text">Message</div>
+    </div>
+  );
+};
 
 const Sidebar = () => {
-  return <div className="sidebar">Hello Sidebar</div>;
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <div className="sidebar">
+      <aside style={{ width: "100%" }}>
+        <DraggableNode onDragStart={onDragStart} nodeType="text" />
+      </aside>
+      {/* Later we can add more components like this by defining 
+          more type of nodes and make it more extensible */}
+    </div>
+  );
 };
 
 export default Sidebar;
